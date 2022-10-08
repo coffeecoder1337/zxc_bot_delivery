abb405a66968c068f1cecf615d7522b35248c14e
Stepa) <stepanderdichok@gmail.com>
2022-10-09 00:21:16 +0300

    basket)

--- tgbot/handlers/start_delivery_handler.py
+++ tgbot/handlers/start_delivery_handler.py
@@ -1,31 +1,33 @@
 import json
+import re
 
 from aiogram import Dispatcher
 from aiogram.types import CallbackQuery
 
 from aiogram.dispatcher import FSMContext
 
-from tgbot.keyboards.inline.callback_datas import menu_callback, menu_vkusochka_callback
+from tgbot.keyboards.inline.callback_datas import menu_callback, menu_vkusochka_callback, basket_callback
 from tgbot.keyboards.inline.menu_buttons import inicialization_delivery, sets_by_restaraunt, food_by_category, \
-    menu_keyboard
+    menu_keyboard, basket_back
 
 from tgbot.keyboards.inline.callback_datas import inicialization_delivery_callback
 from tgbot.misc.states import MenuStateVkusochka
 
 
-async def check_basket(call: CallbackQuery, state: FSMContext):
+async def check_basket(call: CallbackQuery):
     with open('basket.json', 'r', encoding='utf-8') as f:
         basket = json.load(f)
+        f.close()
         answer = ''
         for people in basket:
-            out_string = f'{people}:\n'
+            out_string = f'@{people}:\n'
             for item in basket[people]:
-                out_string += basket[people][item][0] + " " + basket[people][item][0] + "\n"
+                out_string += str(item[0]) + " " + str(item[1][0]) + "\n"
             answer += out_string + "\n"
     if answer != '':
-        await call.message.answer(answer)
+        await call.message.edit_text(answer, reply_markup=basket_back)
     else:
-        await call.message.answer("Корзина пустая")
+        await call.message.edit_text("Корзина пустая", reply_markup=basket_back)
 
 
 async def make_delivery(call: CallbackQuery, state: FSMContext):
@@ -69,7 +71,32 @@ async def make_delivery_restaraunts(call: CallbackQuery, state: FSMContext):
 
 async def delivery_restaraunts_category(call: CallbackQuery, state: FSMContext):
     await call.message.edit_text("Выебите блюдо)", reply_markup=food_by_category(call.data.split(':')[1].strip()))
+    await state.update_data(category=call.data.split(':')[1].strip())
     await state.set_state(MenuStateVkusochka.Q2)
+
+
+async def add_to_basket(call: CallbackQuery, state: FSMContext):
+    food = call.data.split(':')[1]
+    data = await state.get_data()
+    category = data.get('category')
+    with open("menu.json", "r", encoding='utf-8') as file:
+        vkusochka_menu = json.load(file)
+        file.close()
+        for categorys in vkusochka_menu:
+            if str(vkusochka_menu[categorys][0]) == str(category):
+                choosen_category = categorys
+        for item in vkusochka_menu[choosen_category][1::]:
+            if str(item[0][0]) == str(food):
+                with open('basket.json', 'r', encoding='utf-8') as basket:
+                    basket_old = json.load(basket)
+                    basket.close()
+                with open('basket.json', 'w', encoding='utf-8') as basket:
+                    if str(call.from_user.username) in basket_old.keys():
+                        pass
+                    else:
+                        basket_old[str(call.from_user.username)] = []
+                    basket_old[str(call.from_user.username)].append([item[0][1], re.findall(r'\d+', item[1])])
+                    basket.write(json.dumps(basket_old, ensure_ascii=False))
 
 
 async def delivery_restaraunts_category_back(call: CallbackQuery, state: FSMContext):
@@ -96,11 +123,35 @@ async def make_delivery_back(call: CallbackQuery, state: FSMContext):
     await state.finish()
 
 
+async def delivery_start(call: CallbackQuery, state: FSMContext):
+    await call.message.edit_text("Главное меню", reply_markup=menu_keyboard)
+    await state.finish()
+
+
+async def change_sub_status(call: CallbackQuery):
+    with open('subscription.json', 'r', encoding='utf-8') as f:
+        subscribers = json.load(f)
+        f.close()
+        for subscriber in subscribers:
+            if str(subscriber) == str(call.from_user.id):
+                if subscribers[subscriber]:
+                    subscribers[subscriber] = False
+                    await call.answer(text="Подписка отключена", show_alert=True)
+                else:
+                    subscribers[subscriber] = True
+                    await call.answer(text="Подписка подключена", show_alert=True)
+                with open("subscription.json", "w", encoding='utf-8') as file:
+                    json.dump(subscribers, file, ensure_ascii=False)
+
+
 def register_make_delivery(dp: Dispatcher):
+    dp.register_callback_query_handler(change_sub_status, menu_callback.filter(choiсe="disconnect_me"), state=None)
     dp.register_callback_query_handler(make_delivery, menu_callback.filter(choiсe="start_delivery"), state=None)
+    dp.register_callback_query_handler(delivery_start, basket_callback.filter(step="back"), state=None)
     dp.register_callback_query_handler(check_basket, menu_callback.filter(choiсe="basket"), state=None)
     dp.register_callback_query_handler(make_delivery_restaraunts, inicialization_delivery_callback.filter(choiсe="inicialize"), state=None)
     dp.register_callback_query_handler(dont_make_delivery_restaraunts, inicialization_delivery_callback.filter(choiсe="no_inicialize"), state=None)
     dp.register_callback_query_handler(make_delivery_back, menu_vkusochka_callback.filter(category="back"), state=MenuStateVkusochka.Q1)
     dp.register_callback_query_handler(delivery_restaraunts_category_back, menu_vkusochka_callback.filter(category="back"), state=MenuStateVkusochka.Q2)
+    dp.register_callback_query_handler(add_to_basket, state=MenuStateVkusochka.Q2)
     dp.register_callback_query_handler(delivery_restaraunts_category, state=MenuStateVkusochka.Q1)
--- tgbot/handlers/start_handler.py
+++ tgbot/handlers/start_handler.py
@@ -9,17 +9,17 @@ from tgbot.keyboards.inline.menu_buttons import menu_keyboard
 async def delivery_start(message: Message):
     with open("subscription.json", "r", encoding='utf-8') as file:
         all_subs = json.load(file)
+        file.close()
         is_subscriber = False
         for sub in all_subs:
-            if sub == message.from_id and all_subs[sub]:
+            if str(sub) == str(message.from_id) and all_subs[sub]:
                 is_subscriber = True
         if not is_subscriber:
-            sub = dict()
-            sub[message.from_id] = True
+            all_subs[message.from_id] = True
             with open('subscription.json', 'w', encoding='utf-8') as f:
-                json.dump(sub, f, ensure_ascii=False)
+                f.write(json.dumps(all_subs, ensure_ascii=False))
     await message.answer("Приветик)\nГлавное меню", reply_markup=menu_keyboard)
 
 
 def register_delivery_start(dp: Dispatcher):
-    dp.register_message_handler(delivery_start, commands=["menu", "start"], state="*")
+    dp.register_message_handler(delivery_start, commands=["menu", "start"], state=None)
